9e033f1ffff33ef4e31774e60f5441eeaa898812
abhidemon <abhishek.singh@unbxd.com>
2018-01-19 22:46:43 +0530

    with more logs.

--- schemalessdemo/trainmode.py
+++ schemalessdemo/trainmode.py
@@ -1,13 +1,3 @@
-# coding: utf-8
-
-# In[12]:
-
-
-
-
-
-# In[ ]:
-
 
 from time import sleep as g
 import requests
@@ -29,25 +19,7 @@ def prt(*args):
 
 
 def ln():
-    prt("===================================================")
-
-
-def ftable(tbl, pad='  ', sep='\n', normalize=str):
-    # normalize the content to the most useful data type
-    strtbl = [[normalize(it) for it in row] for row in tbl]
-
-    # next, for each column we compute the maximum width needed
-    w = [0 for _ in tbl[0]]
-    for row in strtbl:
-        for ncol, it in enumerate(row):
-            w[ncol] = max(w[ncol], len(it))
-
-    # a string is built iterating on the rows and the items of `strtbl`:
-    #   items are  prepended white space to an uniform column width
-    #   formatted items are `join`ed using `pad` (by default "  ")
-    #   eventually we join the rows using newlines and return
-    return sep.join(pad.join(' ' * (wid - len(it)) + it for wid, it in zip(w, row))
-                    for row in strtbl)
+    prt("==================================================================================")
 
 
 def getStRepr(entry):
@@ -55,7 +27,7 @@ def getStRepr(entry):
         multiValued = "multiValued"
     else:
         multiValued = "singleValued"
-    return  "(" + str(entry['fieldType']) + "," + multiValued + ")"
+    return  "(" + str(entry['type']) + "," + multiValued + ")"
 
 def analyseSchema(prevSchema, curSchema):
     added = []
@@ -65,12 +37,23 @@ def analyseSchema(prevSchema, curSchema):
         for pentry in prevSchema:
             if pentry['name'] == entry['name']:
                 isnew = False
-                if pentry['fieldType'] != entry['fieldType']:
-                    changed.append(entry['name'] + " : " + getStRepr(pentry) + " -> " + getStRepr(entry))
+                if pentry['type'] != entry['type']:
+                    changed.append(str(entry['name']) + " : " + getStRepr(pentry) + " -> " + getStRepr(entry))
         if isnew:
-            added.append( str(entry['name']) + getStRepr(entry) )
+            added.append( str(entry['name']) + " : " +"\t" + getStRepr(entry) )
 
-    return (added, changed)
+    addedStr = ""
+    for addded1 in added:
+        addedStr+="\n\t"+addded1
+    if (addedStr==""):
+        addedStr="None"
+
+    changedStr = ""
+    for ch in changed:
+        changedStr+="\n\t"+ch
+    if (changedStr==""):
+        changedStr="None"
+    return (addedStr, changedStr)
 
 
 # In[ ]:
@@ -90,7 +73,7 @@ START_API = "http://localhost:8983/solr/{}/schema/train/start"
 
 resp = requests.get(CREATE_CORE_API.format(collName))
 prt(json.dumps(json.loads(resp.content)))
-prt("\n>Collection Created!!\n")
+prt("\n>Collection Created : {}\n".format(collName))
 prt(">Creating A Training Session...")
 START_API = START_API.format(collName)
 prt("\n\tcurl ", "'" + START_API + "'")
@@ -115,9 +98,10 @@ prt(">Now, training SOLR with our products....")
 trainURL = "http://localhost:8983/solr/{}/schema/train".format(collName)
 yieldUrl = "http://localhost:8983/solr/{}/schema/train/yield".format(collName)
 dataFile = [
-    {"title": "How to Train Your Schema", "price": 23, "cur": "USD", "qty": 1000, "sku": "2017-01-18"},
-    {"title": "Be Sane, Like Always", "price": 456.4, "cur": "EUR", "qty": 1000, "sku": "b-1122323"},
-    {"name": "Alex Marbles", "favourites": ["bottles", "brews", "warts"]}
+    {"title": "How to Train Your Schema", "price": 23, "cur": "USD", "qty": 1000, "sku": "2017-01-18", "isAvailable":"true"},
+    {"title": "Be Sane, Like Always", "price": 456.4, "cur": "EUR", "qty": 1000, "sku": "b-1122323", "rank":"2011"},
+    {"name" : "Alex Marbles", "favourites": ["bottles", 9999, "warts"], "rank":[201, 2999.334]},
+    {"name" : "Arjan Kant", "favourites": ["ppp", "qwerty", "warts"], "rank": [201, 2999.334], "isAvailable":"false"}
 ]
 
 cnt = 1
@@ -126,12 +110,11 @@ resp = requests.get(yieldUrl, params={"schemaTrainingId": schema_training_id, "i
 respc = json.loads(resp.content)
 prevSchema = respc['schema']['add-field-type']
 prt(">Response: ", json.dumps(respc, indent=4))
-
-
-# raw_input()
+raw_input()
 
 def aassd(data, prevSchema):
     # prt(cnt, ". \tEntry : ", data)
+    ln()
     prt(">Sending Entry:\n\t curl -X POST -d '", data, "'\t", trainURL + "?schemaTrainingId=" + schema_training_id)
     resp = requests.post(trainURL, params={"schemaTrainingId": schema_training_id, "indent": "false"},
                          data=json.dumps([data]))
@@ -149,9 +132,10 @@ def aassd(data, prevSchema):
     prt("Fields Changed: ", changed)
 
     prevSchema = curSchema
-    # raw_input()
+
     # cnt += 1
     ln()
+    raw_input()
     return prevSchema
 
 
